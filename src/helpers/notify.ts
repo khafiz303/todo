f3c0832bebe737c2de07c3da29d129ea1a25f2ef
Садиров Хафиз Хабибович <Hafizs@freedombank.kz>
2025-09-02 17:57:21 +0500

    started notification logyc

---
+++ src/helpers/notify.ts
@@ -0,0 +1,23 @@
+export const requestNotification =  async(): Promise<boolean> => {
+    if(!("Notification" in window)){
+        console.warn('браузер не поддерживает уведомления')
+        return false;
+    }
+
+    let permission = Notification.permission;
+
+    if(permission === 'default'){
+        permission = await Notification.requestPermission();
+    }
+
+    return permission === 'granted';
+}
+
+export const showNotification = (title: string, options?: NotificationOptions) =>{
+    if(!("Notification" in window)) return;
+
+    if(Notification.permission === 'granted'){
+        new Notification(title , options)
+    }
+
+}
