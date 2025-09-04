b30ebeb628bf4b2a95c0ff0b2d431fd34e344dfd
Садиров Хафиз Хабибович <Hafizs@freedombank.kz>
2025-09-04 14:40:22 +0500

    add skeleton and filters

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,9 +1,6 @@
 import {  useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 import type { TypedUseSelectorHook}  from 'react-redux'
-
-// 👇 хук для диспатча (с правильным типом)
 export const useAppDispatch: () => AppDispatch = useDispatch
 
-// 👇 хук для селектора (с правильным типом RootState)
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
