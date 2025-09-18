295ebf28ae431b54b6791898050a8eb0cd90a3ad
Садиров Хафиз Хабибович <Hafizs@freedombank.kz>
2025-09-18 17:27:20 +0500

    nd

--- src/redux/features/toDoSlice.ts
+++ src/redux/features/toDoSlice.ts
@@ -4,43 +4,6 @@ import type { PayloadAction } from '@reduxjs/toolkit'
 interface UiState {
   isAddTaskModalOpen: boolean
   selectedTaskId?: number
-  form: boolean
-}
-
-const initialState: UiState = {
-  isAddTaskModalOpen: false,
-  selectedTaskId: undefined,
-  form: false
-}
-
-export const uiSlice = createSlice({
-  name: 'ui',
-  initialState,
-  reducers: {
-    openAddTaskModal: (state) => {
-      state.isAddTaskModalOpen = true
-    },
-    closeAddTaskModal: (state) => {
-      state.isAddTaskModalOpen = false
-    },
-    setSelectedTaskId: (state, action: PayloadAction<number | undefined>) => {
-      state.selectedTaskId = action.payload
-    },
-    closeSelectForm: (state) => {
-      state.form = false
-    }
-  },
-})
-
-export const { openAddTaskModal, closeAddTaskModal, setSelectedTaskId } = uiSlice.actions
-export default uiSlice.reducer
-
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-
-interface UiState {
-  isAddTaskModalOpen: boolean
-  selectedTaskId?: number
 }
 
 const initialState: UiState = {
